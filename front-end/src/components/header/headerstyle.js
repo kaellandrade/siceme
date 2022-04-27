b3d46407a1a8919abf8f07f65e2009cb9dde72c9
gabriel <marcusgabriel6@gmail.com>
2022-04-27 18:04:45 -0300

    att

--- front-end/src/components/header/headerstyle.js
+++ front-end/src/components/header/headerstyle.js
@@ -12,10 +12,19 @@ export const HeaderStyle = styled.header`
 
 export const ButtonH = styled(Button)`
   background-color: ${props => props.buttonS === props.buttonSelect ? props.backgroundColor : "#316B83" };
-  padding: 16px;
+  padding: 18px;
   font-size: small;
   color: ${props => props.buttonS === props.buttonSelect ? props.colorButton : "#fff" };
   font-weight: 500;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-width: 2px;
+  border-color: #316B83;
+  :hover{
+    border-color: white;
+    background-color: #316B83; 
+  }
 `;
   
 
