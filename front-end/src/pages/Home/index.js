767638d519c42aa40c0a45d279f49c2aea7583d4
gabriel <marcusgabriel6@gmail.com>
2022-04-26 22:53:31 -0300

    att

--- front-end/src/pages/Home/index.js
+++ front-end/src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/header/Header"
 import home from "./home.css"
+import { Input } from "reactstrap"
 
 function Home() {
   return (
@@ -11,13 +12,20 @@ function Home() {
           
           {/* Formulário de busca */}
           <form action="#" className="col-md-12 d-flex align-items-center">
-              <input type="text" name="name" placeholder="Procurar eletrônicos"></input>
+              <input type="text" name="name" placeholder="Buscar kit/material"></input>
               <button className="d-flex align-items-center">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
                       <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                   </svg>
               </button>
           </form>
+
+          <div>
+            <Input  
+              type="search"
+              placeholder="Buscar kit/material" 
+            />
+          </div>
 
       </section>
     </div>
