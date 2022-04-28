fc8aa1c9faf61b13f2af0afc72209ee9b03b62c9
gabriel <marcusgabriel6@gmail.com>
2022-04-28 11:51:56 -0300

    modificado botao de pesquisa

--- front-end/src/pages/Triagem/index.js
+++ front-end/src/pages/Triagem/index.js
@@ -12,27 +12,34 @@ function Main(props) {
     <div className="Main">
       <Header optionButton="Triagem" buttonS={props.buttonS} buttonSelect={props.buttonSelect} />
       <section className="col-md-12 nav d-flex align-items-center justify-content-around">
-          <h1 className="body">Listagem Geral</h1>
-          <ul className="col-md-12 nav d-flex align-items-center justify-content-around">
-
-            <li className="nav-item">
-              <Input  
-                icon="search"
-                className="busca"
-                type="search"
-                placeholder="Buscar kit/material"
-              />
-                  
-            </li>  
+          
+          <div className="container">
             
-            <li>
-              <button className="d-flex align-items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                </svg>
-              </button>
-            </li>
-          </ul>
+            <div className="row">
+              <h1 className="body">Listagem Geral</h1>
+            </div>
+            <div className="row">
+                          
+              <form action="#" className="d-flex align-items-center">
+                  <Input  
+                    icon="search"
+                    className="busca"
+                    type="search"
+                    placeholder="Buscar kit/material"
+                  />
+                  {/* <button >
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                      </svg>
+                  </button> */}
+              </form>
+              
+            </div>
+
+          </div>
+          
+          {/* <ul className="col-md-12 nav d-flex align-items-center justify-content-around"> */}
+
 
           <main className="container flex-column" >
             <div className="row">
