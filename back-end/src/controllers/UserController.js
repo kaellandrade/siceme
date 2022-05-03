ccd941067895bf8ec1dbc599b3df46109d34cab7
root <root@Notebook-Acer.localdomain>
2022-05-03 17:51:47 -0300

    Ajuste da tabela id para uso_id

--- back-end/src/controllers/UserController.js
+++ back-end/src/controllers/UserController.js
@@ -2,11 +2,11 @@ import User from '../app/models/User';
 
 class UserController {
   async store(req, res) {
-    const { id, uso_nome, uso_email, uso_cargo, uso_tipo_user } =
+    const { uso_id, uso_nome, uso_email, uso_cargo, uso_tipo_user } =
       await User.create(req.body);
 
     return res.json({
-      id,
+      uso_id,
       uso_nome,
       uso_email,
       uso_cargo,
