cca171b733108ed0369b502a9dd515832e19dfab
gabriel <marcusgabriel6@gmail.com>
2022-05-04 11:31:34 -0300

    melhorada responsividade dos bottons header

--- front-end/src/components/header/ButtonHeader.jsx
+++ front-end/src/components/header/ButtonHeader.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import {ButtonH} from "./headerstyle.js";
+import { ButtonH } from "./headerstyle.js";
+
 
 
 function ButtonHeader(props) {
   return (
-    <li className="nav-item">
-      <a href={props.href}>
-        <ButtonH className="button" texto={props.texto} 
-                 buttonSelect={props.buttonSelect} >
-          {props.texto}
-        </ButtonH>
-      </a>
-    </li>
+
+    <a href={props.href} className="abuttonHeader" >
+      <ButtonH className="button" texto={props.texto}
+        buttonSelect={props.buttonSelect}
+      >
+        {props.texto}
+      </ButtonH>
+    </a>
+
   );
 }
 
