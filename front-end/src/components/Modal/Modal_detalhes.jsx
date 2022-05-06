7fcb84fde52bb5b695b0cbffc179d33ebd3e782f
gabriel <marcusgabriel6@gmail.com>
2022-05-06 09:00:56 -0300

    add image generica detalhes e removido fabricante

--- front-end/src/components/Modal/Modal_detalhes.jsx
+++ front-end/src/components/Modal/Modal_detalhes.jsx
@@ -32,141 +32,127 @@ function Modal_detalhes(props) {
         <ModalBody>
           <div className="container-fluid">
             <Form inline >
-              <Col md={12}>
-              
-                <Row>
-                  <Col md={5}>
-                    <FormGroup floating>
-                      <Input
-                        id="nome do material"
-                        name="nome"
-                        type="text"
-                        value={props.nome}
-                        readOnly
-                      />
-                      <Label for="nome do material">
-                        Nome do material
-                      </Label>
-                    </FormGroup>
-                  </Col>
-                  <Col md={5}>
-                    <FormGroup floating>
-                      <Input
-                        id="fabricante"
-                        name="name"
-                        type="name"
-                        placeholder="Fabricante"
-                        value={props.fabricante}
-                        readOnly
-                      />
-                      <Label for="fabricante">
-                        Fabricante
-                      </Label>
-                    </FormGroup>
-                  </Col>
-                  <Col md={2}>
-                    <FormGroup floating>
-                      <Input
-                        id="validade"
-                        name="validade"
-                        type="date"
-                        placeholder="Validade"
-                        value={props.validade}
-                        readOnly
-                      />
-                      <Label for="validade">
-                        Validade
-                      </Label>
-                    </FormGroup>
-                  </Col>
-                </Row>
+              <div className="row">
+                <Col md={8}>
+                  <Row>
+                    <Col md={5}>
+                      <FormGroup floating>
+                        <Input
+                          id="nome do material"
+                          name="nome"
+                          type="text"
+                          value={props.nome}
+                          readOnly
+                        />
+                        <Label for="nome do material">
+                          Nome do material
+                        </Label>
+                      </FormGroup>
+                    </Col>
+                    <Col md={4}>
+                      <FormGroup floating>
+                        <Input
+                          id="quantidade"
+                          name="quantidade"
+                          type="number"
+                          min={1}
+                          placeholder="Quantidade"
+                          value={props.qtde}
+                          readOnly
+                        />
+                        <Label for="quantidade">
+                          Quantidade
+                        </Label>
+                      </FormGroup>
+                    </Col>
+                    <Col md={3}>
+                      <FormGroup floating>
+                        <Input
+                          id="validade"
+                          name="validade"
+                          type="date"
+                          placeholder="Validade"
+                          value={props.validade}
+                          readOnly
+                        />
+                        <Label for="validade">
+                          Validade
+                        </Label>
+                      </FormGroup>
+                    </Col>
+                  </Row>
 
-                <Row>
-                  <Col md={4}>
-                    <FormGroup floating>
-                      <Input
-                        id="codigo"
-                        name="codigo"
-                        type="text"
-                        placeholder="codigo"
-                        value={props.codigo}
-                        readOnly
-                      />
-                      <Label for="codigo">
-                        Código
-                      </Label>
-                    </FormGroup>
-                  </Col>
-                  <Col md={4}>
-                    <FormGroup floating>
-                      <Input
-                        id="aplicacao"
-                        name="aplicacao"
-                        type="text"
-                        placeholder="Auantidade"
-                        value={props.aplicacao}
-                        readOnly
-                      />
-                      <Label for="aplicacao">
-                        Aplicação
-                      </Label>
-                    </FormGroup>
-                  </Col>
-                  <Col md={4}>
-                    <FormGroup floating>
-                      <Input
-                        id="status"
-                        name="status"
-                        type="text"
-                        placeholder="Status"
-                        value={props.status}
-                        readOnly
-                      />
-                      <Label for="status">
-                        Status
-                      </Label>
-                    </FormGroup>
-                  </Col>
-                </Row>
+                  <Row>
+                    <Col md={4}>
+                      <FormGroup floating>
+                        <Input
+                          id="codigo"
+                          name="codigo"
+                          type="text"
+                          placeholder="codigo"
+                          value={props.codigo}
+                          readOnly
+                        />
+                        <Label for="codigo">
+                          Código
+                        </Label>
+                      </FormGroup>
+                    </Col>
+                    <Col md={4}>
+                      <FormGroup floating>
+                        <Input
+                          id="aplicacao"
+                          name="aplicacao"
+                          type="text"
+                          placeholder="Auantidade"
+                          value={props.aplicacao}
+                          readOnly
+                        />
+                        <Label for="aplicacao">
+                          Aplicação
+                        </Label>
+                      </FormGroup>
+                    </Col>
+                    <Col md={4}>
+                      <FormGroup floating>
+                        <Input
+                          id="status"
+                          name="status"
+                          type="text"
+                          placeholder="Status"
+                          value={props.status}
+                          readOnly
+                        />
+                        <Label for="status">
+                          Status
+                        </Label>
+                      </FormGroup>
+                    </Col>
+                  </Row>
 
-                <Row>
-                  <Col md={10}>
-                    <FormGroup floating>
-                      <Input
-                        id="descricao"
-                        name="descricao"
-                        type="textarea"
-                        maxLength={250}
-                        placeholder="Descrição do material"
-                        value={props.descricao}
-                        readOnly
-                      />
-                      <Label for="descricao">
-                        Descrição do material
-                      </Label>
-                    </FormGroup>
-                  </Col>
-                  <Col md={2}>
-                    <FormGroup floating>
-                      <Input
-                        id="quantidade"
-                        name="quantidade"
-                        type="number"
-                        min={1}
-                        placeholder="Quantidade"
-                        value={props.qtde}
-                        readOnly
-                      />
-                      <Label for="quantidade">
-                        Quantidade
-                      </Label>
-                    </FormGroup>
-                  </Col>
-                </Row>
-              </Col>
-              <Col md={4}>
-                <img src="../../../public/image-generica-material.jpg"></img>
-              </Col>
+                  <Row>
+                    <Col md={12}>
+                      <FormGroup floating>
+                        <Input
+                          id="descricao"
+                          name="descricao"
+                          type="textarea"
+                          maxLength={250}
+                          placeholder="Descrição do material"
+                          value={props.descricao}
+                          readOnly
+                        />
+                        <Label for="descricao">
+                          Descrição do material
+                        </Label>
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                </Col>
+                <Col md={4}>
+                  <img src="https://i.imgur.com/OCvs0SP.jpg" class="img-thumbnail" alt="material cirúrgico"/>
+                </Col>
+              </div>
             </Form>
           </div>
         </ModalBody>
