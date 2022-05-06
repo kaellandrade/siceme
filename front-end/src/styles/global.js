826384029c03df066c2be888bd2616bf8b7281b5
gabriel <marcusgabriel6@gmail.com>
2022-05-06 09:29:51 -0300

    mudado height da col md 1 pra vh

--- front-end/src/styles/global.js
+++ front-end/src/styles/global.js
@@ -5,7 +5,7 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0px;
     outline: 0;
-    box-sizing: border-box;    
+    box-sizing: border-box;
     /* overflow: hidden; //desativar barra de rolagem */
   }
   
