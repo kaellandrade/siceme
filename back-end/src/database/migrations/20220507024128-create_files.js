4afc53a4e746c6e726677fb80bb0fb56b3bbc901
Manoel Moreira <manoelwp@outlook.com>
2022-05-08 19:44:48 -0300

    migration de criação de arquivo

---
+++ back-end/src/database/migrations/20220507024128-create_files.js
@@ -0,0 +1,34 @@
+'use_strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('ars_arquivos', {
+      ars_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      ars_nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      ars_path: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('ars_arquivos');
+  },
+};
