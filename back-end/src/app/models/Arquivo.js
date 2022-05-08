58ef2b4e9d00772fbe7151ff6ee9a438cb9987ef
Manoel Moreira <manoelwp@outlook.com>
2022-05-08 19:43:57 -0300

    Criação do model arquivo

---
+++ back-end/src/app/models/Arquivo.js
@@ -0,0 +1,27 @@
+import Sequelize, { Model } from 'sequelize';
+
+/**
+ * Criação do model usuário
+ * @author Manoel
+ * @version 1.0.0 Realizando conexão
+ */
+class Arquivo extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        ars_id: Sequelize.INTEGER,
+        ars_nome: Sequelize.STRING,
+        ars_path: Sequelize.STRING,
+      },
+      {
+        sequelize,
+        tableName: 'ars_arquivos',
+      },
+    );
+    this.removeAttribute('id');
+
+    return this;
+  }
+}
+
+export default Arquivo;
