4a4728bde2db7676aa751366898d2fd6aff2f457
gabriel <marcusgabriel6@gmail.com>
2022-05-12 11:18:08 -0300

    modificado padrao de status no data.js

--- front-end/src/data/data.js
+++ front-end/src/data/data.js
@@ -5,7 +5,7 @@ export const materiais = [
     "nome": "Kit de emergencia medica",
     "qtde": "85",
     "aplicacao": "Cirurgia em geral",
-    "status": "Distribuição"
+    "status": "Desinfecção - Processando"
   },
   {
     "id": "2",
@@ -13,15 +13,15 @@ export const materiais = [
     "nome": "Kit médico oftalmológico",
     "qtde": "45",
     "aplicacao": "Cirurgia oftalmológica",
-    "status": "Desinfecção"
+    "status": "Desinfecção - Pronto"
   },
   {
-    "id": "5",
+    "id": "3",
     "codigo": "002341",
     "nome": "Pinça Schimidt Reta 18cm",
     "qtde": "41",
     "aplicacao": "Cirurgia de amígdalas",
-    "status": "Esterilização"
+    "status": "Esterilização - Processando"
   },
   {
     "id": "4",
@@ -29,265 +29,24 @@ export const materiais = [
     "nome": "Agulha 21G 1/2 40x8",
     "qtde": "29",
     "aplicacao": "Raqueanestesia",
-    "status": "Limpeza"
+    "status": "Esterilização - Pronto"
   },
   {
-    "id": "1",
+    "id": "5",
     "codigo": "001817",
     "nome": "Kit de emergencia medica",
     "qtde": "85",
     "aplicacao": "Cirurgia em geral",
-    "status": "Distribuição"
+    "status": "Limpeza - Processando"
   },
   {
-    "id": "2",
+    "id": "6",
     "codigo": "001487",
     "nome": "Kit médico oftalmológico",
     "qtde": "45",
     "aplicacao": "Cirurgia oftalmológica",
-    "status": "Desinfecção"
-  },
-  {
-    "id": "5",
-    "codigo": "002341",
-    "nome": "Pinça Schimidt Reta 18cm",
-    "qtde": "41",
-    "aplicacao": "Cirurgia de amígdalas",
-    "status": "Esterilização"
-  },
-  {
-    "id": "4",
-    "codigo": "001343",
-    "nome": "Agulha 21G 1/2 40x8",
-    "qtde": "29",
-    "aplicacao": "Raqueanestesia",
-    "status": "Limpeza"
-  },
-  {
-    "id": "1",
-    "codigo": "001817",
-    "nome": "Kit de emergencia medica",
-    "qtde": "85",
-    "aplicacao": "Cirurgia em geral",
-    "status": "Distribuição"
-  },
-  {
-    "id": "2",
-    "codigo": "001487",
-    "nome": "Kit médico oftalmológico",
-    "qtde": "45",
-    "aplicacao": "Cirurgia oftalmológica",
-    "status": "Desinfecção"
-  },
-  {
-    "id": "5",
-    "codigo": "002341",
-    "nome": "Pinça Schimidt Reta 18cm",
-    "qtde": "41",
-    "aplicacao": "Cirurgia de amígdalas",
-    "status": "Esterilização"
-  },
-  {
-    "id": "4",
-    "codigo": "001343",
-    "nome": "Agulha 21G 1/2 40x8",
-    "qtde": "29",
-    "aplicacao": "Raqueanestesia",
-    "status": "Limpeza"
-  },
-  {
-    "id": "1",
-    "codigo": "001817",
-    "nome": "Kit de emergencia medica",
-    "qtde": "85",
-    "aplicacao": "Cirurgia em geral",
-    "status": "Distribuição"
-  },
-  {
-    "id": "2",
-    "codigo": "001487",
-    "nome": "Kit médico oftalmológico",
-    "qtde": "45",
-    "aplicacao": "Cirurgia oftalmológica",
-    "status": "Desinfecção"
-  },
-  {
-    "id": "5",
-    "codigo": "002341",
-    "nome": "Pinça Schimidt Reta 18cm",
-    "qtde": "41",
-    "aplicacao": "Cirurgia de amígdalas",
-    "status": "Esterilização"
-  },
-  {
-    "id": "4",
-    "codigo": "001343",
-    "nome": "Agulha 21G 1/2 40x8",
-    "qtde": "29",
-    "aplicacao": "Raqueanestesia",
-    "status": "Limpeza"
-  },
-  {
-    "id": "1",
-    "codigo": "001817",
-    "nome": "Kit de emergencia medica",
-    "qtde": "85",
-    "aplicacao": "Cirurgia em geral",
-    "status": "Distribuição"
-  },
-  {
-    "id": "2",
-    "codigo": "001487",
-    "nome": "Kit médico oftalmológico",
-    "qtde": "45",
-    "aplicacao": "Cirurgia oftalmológica",
-    "status": "Desinfecção"
-  },
-  {
-    "id": "5",
-    "codigo": "002341",
-    "nome": "Pinça Schimidt Reta 18cm",
-    "qtde": "41",
-    "aplicacao": "Cirurgia de amígdalas",
-    "status": "Esterilização"
-  },
-  {
-    "id": "4",
-    "codigo": "001343",
-    "nome": "Agulha 21G 1/2 40x8",
-    "qtde": "29",
-    "aplicacao": "Raqueanestesia",
-    "status": "Limpeza"
-  },
-  {
-    "id": "1",
-    "codigo": "001817",
-    "nome": "Kit de emergencia medica",
-    "qtde": "85",
-    "aplicacao": "Cirurgia em geral",
-    "status": "Distribuição"
-  },
-  {
-    "id": "2",
-    "codigo": "001487",
-    "nome": "Kit médico oftalmológico",
-    "qtde": "45",
-    "aplicacao": "Cirurgia oftalmológica",
-    "status": "Desinfecção"
-  },
-  {
-    "id": "5",
-    "codigo": "002341",
-    "nome": "Pinça Schimidt Reta 18cm",
-    "qtde": "41",
-    "aplicacao": "Cirurgia de amígdalas",
-    "status": "Esterilização"
-  },
-  {
-    "id": "4",
-    "codigo": "001343",
-    "nome": "Agulha 21G 1/2 40x8",
-    "qtde": "29",
-    "aplicacao": "Raqueanestesia",
-    "status": "Limpeza"
-  },
-  {
-    "id": "1",
-    "codigo": "001817",
-    "nome": "Kit de emergencia medica",
-    "qtde": "85",
-    "aplicacao": "Cirurgia em geral",
-    "status": "Distribuição"
-  },
-  {
-    "id": "2",
-    "codigo": "001487",
-    "nome": "Kit médico oftalmológico",
-    "qtde": "45",
-    "aplicacao": "Cirurgia oftalmológica",
-    "status": "Desinfecção"
-  },
-  {
-    "id": "5",
-    "codigo": "002341",
-    "nome": "Pinça Schimidt Reta 18cm",
-    "qtde": "41",
-    "aplicacao": "Cirurgia de amígdalas",
-    "status": "Esterilização"
-  },
-  {
-    "id": "4",
-    "codigo": "001343",
-    "nome": "Agulha 21G 1/2 40x8",
-    "qtde": "29",
-    "aplicacao": "Raqueanestesia",
-    "status": "Limpeza"
-  },
-  {
-    "id": "1",
-    "codigo": "001817",
-    "nome": "Kit de emergencia medica",
-    "qtde": "85",
-    "aplicacao": "Cirurgia em geral",
-    "status": "Distribuição"
-  },
-  {
-    "id": "2",
-    "codigo": "001487",
-    "nome": "Kit médico oftalmológico",
-    "qtde": "45",
-    "aplicacao": "Cirurgia oftalmológica",
-    "status": "Desinfecção"
-  },
-  {
-    "id": "5",
-    "codigo": "002341",
-    "nome": "Pinça Schimidt Reta 18cm",
-    "qtde": "41",
-    "aplicacao": "Cirurgia de amígdalas",
-    "status": "Esterilização"
-  },
-  {
-    "id": "4",
-    "codigo": "001343",
-    "nome": "Agulha 21G 1/2 40x8",
-    "qtde": "29",
-    "aplicacao": "Raqueanestesia",
-    "status": "Limpeza"
-  },
-  {
-    "id": "1",
-    "codigo": "001817",
-    "nome": "Kit de emergencia medica",
-    "qtde": "85",
-    "aplicacao": "Cirurgia em geral",
-    "status": "Distribuição"
-  },
-  {
-    "id": "2",
-    "codigo": "001487",
-    "nome": "Kit médico oftalmológico",
-    "qtde": "45",
-    "aplicacao": "Cirurgia oftalmológica",
-    "status": "Desinfecção"
-  },
-  {
-    "id": "5",
-    "codigo": "002341",
-    "nome": "Pinça Schimidt Reta 18cm",
-    "qtde": "41",
-    "aplicacao": "Cirurgia de amígdalas",
-    "status": "Esterilização"
-  },
-  {
-    "id": "4",
-    "codigo": "001343",
-    "nome": "Agulha 21G 1/2 40x8",
-    "qtde": "29",
-    "aplicacao": "Raqueanestesia",
-    "status": "Limpeza"
+    "status": "Limpeza - Pronto"
   }
-  
 ];
 
 export const categorias = [
