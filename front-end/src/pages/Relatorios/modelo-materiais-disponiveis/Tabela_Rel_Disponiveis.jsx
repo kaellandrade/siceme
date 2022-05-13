04a3397870595f3709947849c769405b3b32bfe1
gabriel <marcusgabriel6@gmail.com>
2022-05-13 00:09:32 -0300

    add imprimir relatorio materiais disponiveis

---
+++ front-end/src/pages/Relatorios/modelo-materiais-disponiveis/Tabela_Rel_Disponiveis.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Table } from "reactstrap";
+import RowTabela_Rel_Disponiveis from "./RowTabela_Rel_Disponiveis";
+import "./tabela_relatorio.css"
+
+function Tabela_Rel_Disponiveis(props) {
+
+  var materiais = props.dataMaterias;
+
+  const renderMateriais = (materiais) => {
+    return (
+      materiais.map(({ id, codigo, nome, qtde, aplicacao, status}) => {
+        if(status === "Esterilização - Pronto"){
+          return <RowTabela_Rel_Disponiveis id={id} codigo={codigo} nome={nome} 
+                    qtde={qtde} aplicacao={aplicacao} status={status} />
+        }
+      })
+    )
+  } 
+
+  return (
+    <>
+      <div class="table-responsive-relatorio">
+        <Table
+          borderless
+          hover
+          responsive
+          striped
+          id="tabela-relatorio"
+        >
+          <thead>
+            <tr>
+              <th>
+                Código
+              </th>
+              <th>
+                Nome
+              </th>
+              <th>
+                Quantidade
+              </th>
+              <th>
+                Aplicação
+              </th>
+              <th>
+                Status
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {renderMateriais(materiais)}
+          </tbody>
+        </Table>
+      </div>
+    </>
+  );
+}
+
+export default Tabela_Rel_Disponiveis;
