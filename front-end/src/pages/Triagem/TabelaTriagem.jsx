605b5aa315b3f3c0369afd29545b7ca1b0d04322
gabriel <marcusgabriel6@gmail.com>
2022-05-15 17:23:16 -0300

    add tratamento button select tabela triagem

--- front-end/src/pages/Triagem/TabelaTriagem.jsx
+++ front-end/src/pages/Triagem/TabelaTriagem.jsx
@@ -7,6 +7,38 @@ function TabelaTriagem(props) {
   var materiais = props.dataMaterias;
   var buttonSelect = props.buttonSelect;
 
+  const tratarButtonSelect = (buttonSelect) => {
+    switch (buttonSelect) {
+      case 'Limpeza - Processando':
+        buttonSelect = 'LIMPEZA:Processando';
+        break;
+      case 'Limpeza - Pronto':
+        buttonSelect = 'LIMPEZA:PRONTO';
+        break;
+      case 'Desinfecção - Processando':
+        buttonSelect = 'DESINFECÇÃO:PROCESSANDO';
+        break;
+      case 'Desinfecção - Pronto':
+        buttonSelect = 'DESINFECÇÃO:PRONTO';
+        break;
+      case 'Esterilização - Processando':
+        buttonSelect = 'ESTERILIZAÇÃO:PROCESSANDO';
+        break;
+      case 'Esterilização - Pronto':
+        buttonSelect = 'ESTERILIZAÇÃO:PRONTO';
+        break;
+      case 'Entregue':
+        buttonSelect = 'ENTREGUE';
+        break;
+      case 'Devolvido':
+        buttonSelect = 'DEVOLVIDO';
+        break;
+      
+      // default:
+      //   buttonSelect = 'Limpeza - Processando';
+    }
+  }
+
   const renderMateriais = (materiais) => {
     if(buttonSelect == "Listagem Geral"){
       return (
@@ -19,10 +51,11 @@ function TabelaTriagem(props) {
     }
     else{
       return (
-        materiais.map(({ id, codigo, nome, qtde, aplicacao, status, solicitante}) => {
-          if(status === buttonSelect){
-            return <RowTabelaTriagem id={id} codigo={codigo} nome={nome} 
-                      qtde={qtde} aplicacao={aplicacao} status={status} solicitante={solicitante} />
+        materiais.map(({ id, mtl_codigo, mtl_nome, mtl_quantidade, mtl_categoria, mtl_status_id, mtl_descricao,solicitante}) => {
+          if(mtl_status_id === buttonSelect){
+            return <RowTabelaTriagem id={id} codigo={mtl_codigo} nome={mtl_nome} 
+                    qtde={mtl_quantidade} aplicacao={mtl_categoria} status={mtl_status_id} 
+                    solicitante={solicitante} descricao={mtl_descricao} />
           }
         })
       )
