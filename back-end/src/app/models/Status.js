3e187ce6f5014ecb3e08c0e6bcd99d3a1c7be4d6
Micael Andrade <micaelandrade@moobitech.com.br>
2022-05-15 18:19:34 -0300

    doc

--- back-end/src/app/models/Status.js
+++ back-end/src/app/models/Status.js
@@ -1,5 +1,11 @@
 import Sequelize, { Model } from 'sequelize';
 
+/**
+ * Model Referente aos Status
+ * @author Manoel e Micael
+ * @version 1.0.0 Adicionando os campos básicos de material
+ * @version 1.0.1 Id inferido automaticamente pelo Sequelize
+ */
 class Status extends Model {
   static init(sequelize) {
     super.init(
