3ef3586a7fd38617814582cd21a84bfc3984a925
Micael Andrade <micaelandrade@moobitech.com.br>
2022-05-16 01:10:25 -0300

    listagem todas categorias

--- back-end/src/controllers/CategoriaController.js
+++ back-end/src/controllers/CategoriaController.js
@@ -7,8 +7,8 @@ import Categoria from '../app/models/Categoria';
  */
 class CategoriaController {
   async index(req, res) {
-    const { id, cta_nome } = await Categoria.findByPk(req.params.id);
-    return res.status(200).json({ id, cta_nome });
+    const categorias = await Categoria.findAll();
+    return res.status(200).json(categorias);
   }
 
   async store(req, res) {
